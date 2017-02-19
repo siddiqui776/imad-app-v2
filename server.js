396b823594164ae900e58729ff80b53586fe74a9
siddiqui776 <asimsiddiqui776@gmail.com>
2017-02-19 12:34:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/artice one',function (req,res){
+app.get('/artice one',function (req, res){
  res.send('article-one will requested and serve here'); 
 });
 app.get('/artice-two',function (req, res){
