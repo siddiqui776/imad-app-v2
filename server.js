2c56c5de1ba65bbba9ccd696cf085cc84c36d21e
siddiqui776 <asimsiddiqui776@gmail.com>
2017-02-19 12:27:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,13 +8,20 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/artice one',function (req,res){
+ res.send('article one will requested and serve here'); 
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+ });
+app.get('/artice two',function (req,res){
+ res.send('article two will requested and serve here');
 });
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+app.get('/artice three',function (req,res){
+ res.send('article three will requested and serve here');
 });
 
 
