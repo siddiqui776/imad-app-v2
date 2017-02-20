43f670eb662f0e7f80cb14d6acc438bc0491f6c2
siddiqui776 <asimsiddiqui776@gmail.com>
2017-02-20 20:50:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article one', function (req, res){
- res.send('article-one will requested and serve here'); 
+ res.send('article-one will requested and serve here','ui', 'article-one.html')
+ ; 
 });
 
 app.get('/article-two', function (req, res){
